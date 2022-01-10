04d046c4855736750a8efca8a7cb6743a3288f44
zaobr <88708725+zaobr@users.noreply.github.com>
2022-01-10 18:10:32 +0100

    Cart update

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -1,31 +1,35 @@
 const cartItems = document.getElementById("cart__items");
 const cartList = JSON.parse(localStorage.getItem("cart"));
 
-fetch(`http://localhost:3000/api/products/${searchParam}`)
-    .then(response => response.json())
-    .then(data => {displayCartItems(data)});
+console.log(typeof JSON.parse(localStorage.getItem("cart")))
 
-function displayCartItems(data) {
-  cartItems.innerHTML = 
-        `<article class="cart__item" data-id="${localStorage.getItem("id")}" data-color="${localStorage.getItem("color")}">
-        <div class="cart__item__img">
-          <img src="${data.imageUrl}" alt="${data.altTxt}">
-        </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>${data.name}</h2>
-            <p>${localStorage.getItem("color")}</p>
-            <p>${data.price} €</p>
-          </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localStorage.getItem("quantity")}">
-            </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
-            </div>
-          </div>
-        </div>
-      </article>`  
+for (let index = cartList.length; index > 0; index--) {
+  displayData(index-1);
+}
+
+function displayData(index) {
+  fetch(`http://localhost:3000/api/products/${cartList[index].id}`)
+  .then(response => response.json())
+  .then(data => {cartItems.innerHTML = `<article class="cart__item" data-id="${data.id}" data-color="${cartList[index].color}">
+    <div class="cart__item__img">
+    <img src="${data.imageUrl}" alt="${data.altTxt}">
+    </div>
+    <div class="cart__item__content">
+    <div class="cart__item__content__description">
+    <h2>${data.name}</h2>
+    <p>${cartList[index].color}</p>
+    <p>${data.price} €</p>
+    </div>
+    <div class="cart__item__content__settings">
+    <div class="cart__item__content__settings__quantity">
+    <p>Qté : </p>
+    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartList[index].quantity}">
+    </div>
+    <div class="cart__item__content__settings__delete">
+    <p class="deleteItem">Supprimer</p>
+    </div>
+    </div>
+    </div>
+    </article>`
+  })
 }
